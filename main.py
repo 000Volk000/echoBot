fac32f778aff519dc195669603e2df1d26311fd7
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-08-03 11:29:38 +0200

    feat: Hybrid command saluda added and working

--- main.py
+++ main.py
@@ -17,13 +17,18 @@ intents.message_content = True
 intents.members = True
 
 # Initialize bot
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 # Handling events
 ## Event when the bot is ready
 @bot.event
 async def on_ready():
     print(f'Estamos ready para funcionar, {bot.user.name}')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(f"Failed to sync commands: {e}")
 
 ## Event when a new member joins
 @bot.event
@@ -38,12 +43,23 @@ async def on_message(message):
 
     if "hola" in message.content.lower():
         await message.delete()
-        await message.channel.send(f"¿De que vas {message.author.mention}?, aquí el unico que saluda soy yo")
+        await message.channel.send(f"¿De que vas {message.author.mention}?, aquí el unico que saluda soy yo.\n\nSi quieres saludar a alguien, usa el comando `/saluda`")
 
     if "jaime" in message.content.lower() or f"<@{390909965038125059}>" in message.content or f"<@!{390909965038125059}>" in message.content:
         await message.reply("<:uh:1391363166910283799>")
 
     await bot.process_commands(message)
+
+# Handling commands
+## Command to greet a user or in general
+@bot.hybrid_command(name="saluda", description="👋 Saluda a otro usuario del servidor o en general")
+async def saluda(ctx: commands.Context, usuario: discord.Member = None):
+    if usuario is None:
+        message = f"{ctx.author.mention} quiere saludar en general"
+    else:
+        message = f"{ctx.author.mention} te quiere saludar {usuario.mention}"
+
+    await ctx.send(message)
 
 # Run the bot
 if __name__ == "__main__":
