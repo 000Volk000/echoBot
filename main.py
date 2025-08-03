4d59b7e9eca5c8c51cd50189c57403a02a0226b5
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-08-03 10:22:10 +0200

    feat: Basic bot created only with on_ready

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+# Set up basic logging
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+
+# Set up intents
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+# Initialize bot
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+# Handling events
+## Event when the bot is ready
+@bot.event
+async def on_ready():
+    print(f'Estamos ready para funcionar, {bot.user.name}')
+
+# Run the bot
+if __name__ == "__main__":
+    if token:
+        bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+    else:
+        print("Error: DISCORD_TOKEN not found in environment variables.")
+        exit(1)
