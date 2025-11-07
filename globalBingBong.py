77dc8a06451d764bd989008faa78cd6ea4f128c7
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-11-07 15:44:36 +0100

    feat: globalBingBong.py created so it joins, bingbonbs and exit

---
+++ globalBingBong.py
@@ -0,0 +1,75 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+import random
+import asyncio
+import sys
+
+# Load environment variables
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+# Set up basic logging to console
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    stream=sys.stdout,
+)
+
+
+# Set up intents
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+# Initialize bot
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+# Scheduled task for bingbong playing every hour
+async def globalBingBong():
+    for guild in bot.guilds:
+        for vc in guild.voice_channels:
+            if len(vc.members) > 0:
+                voice_client = await vc.connect()
+                music_folder = "sounds/bingbong"
+                music_files = sorted(
+                    [f for f in os.listdir(music_folder) if f.endswith(".mp3")]
+                )
+                selected_song = random.choices(music_files, weights=[99.99, 0.01], k=1)[
+                    0
+                ]
+
+                while not voice_client.is_connected():
+                    await asyncio.sleep(0.1)
+
+                voice_client.play(
+                    discord.FFmpegPCMAudio(os.path.join(music_folder, selected_song))
+                )
+
+                while voice_client.is_playing():
+                    await asyncio.sleep(0.1)
+
+                await voice_client.disconnect()
+
+
+# Handling events
+## Event when the bot is ready
+@bot.event
+async def on_ready():
+    logging.info("Starting global BingBong task:")
+    if await globalBingBong():
+        logging.info("Finished successfully global BingBong Task")
+    else:
+        logging.info("Finished successfully global BingBong Task")
+
+
+# Run the bot
+if __name__ == "__main__":
+    if token:
+        bot.run(token)
+    else:
+        logging.critical("Error: DISCORD_TOKEN not found in environment variables.")
+exit(1)
